b005fcf89918317e337c9b9ef43e479d8e420956
PremPrakashS <premp7420@gmail.com>
2023-12-20 15:36:36 +0100

    modified tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo ">>> installing required packages..."
+pip install -r .\project\requirement.txt
 echo ">>> running the tests ..."
 python3 ./project/automated_testing.py
 echo ">>> tests ran sucessfully..."
