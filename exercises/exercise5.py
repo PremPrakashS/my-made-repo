e54e4a475aeda47cb514bcd52b305bd5a8aa0623
PremPrakashS <premp7420@gmail.com>
2024-01-24 12:46:46 +0100

    Updated exercise5.py

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -17,42 +17,47 @@ def get_source_zip(gtfs_url, gtfs_path):
     return zip_filename
 
 
-gtfs_url = "https://gtfs.rhoenenergie-bus.de/GTFS.zip"
-gtfs_zip_path = "./GTFS.zip"
-file_to_extract = "stops.txt"
+def write_sql_to_df(db_name, table_name, column_dtype):
+    engine = create_engine(f'sqlite:///{db_name}')
+    stops_df.to_sql(table_name, engine, if_exists="replace", index=False, dtype=column_dtype)
+    engine.dispose()
 
-zip_path = get_source_zip(gtfs_url, gtfs_zip_path)
 
-file_path = extract_file_from_zip(zip_path, file_to_extract)
+def delete_files(file_list):
+    for file in files_to_delete:
+        if os.path.exists(file):
+            os.remove(file)
 
-stops_df = pd.read_csv(file_path, usecols=["stop_id", "stop_name", "stop_lat", "stop_lon", "zone_id"])
+if __name__ == '__main__':
+    gtfs_url = "https://gtfs.rhoenenergie-bus.de/GTFS.zip"
+    gtfs_zip_path = "./GTFS.zip"
+    file_to_extract = "stops.txt"
+    files_to_delete = []
+    db_name = "gtfs.sqlite"
+    table_name = "stops"
+    column_types = {
+            'stop_id':INTEGER,
+            'stop_name':TEXT,
+            'stop_lat':REAL,
+            'stop_lon':REAL,
+            'zone_id':INTEGER
+    }
+    
 
-# Filter for zone_id = 2001
-stops_df = stops_df[stops_df["zone_id"] == 2001]
-# for valid longitute and latitude
-valid_coordinates = stops_df["stop_lat"].between(-90, 90) & stops_df["stop_lon"].between(-90, 90)
-stops_df = stops_df[valid_coordinates]
-#drop invalid rows
-stops_df.dropna(inplace=True)
+    zip_path = get_source_zip(gtfs_url, gtfs_zip_path)
 
-engine = create_engine(f'sqlite:///gtfs.sqlite')
+    file_path = extract_file_from_zip(zip_path, file_to_extract)
 
-column_types = {
-        'stop_id':INTEGER,
-        'stop_name':TEXT,
-        'stop_lat':REAL,
-        'stop_lon':REAL,
-        'zone_id':INTEGER
-}
+    stops_df = pd.read_csv(file_path, usecols=["stop_id", "stop_name", "stop_lat", "stop_lon", "zone_id"])
+    stops_df = stops_df[stops_df["zone_id"] == 2001]        # Filter for zone_id = 2001
+    valid_coordinates = stops_df["stop_lat"].between(-90, 90) & stops_df["stop_lon"].between(-90, 90)    # filter for valid longitute and latitude
+    stops_df = stops_df[valid_coordinates]
+    stops_df.dropna(inplace=True)       # drop invalid rows
 
-stops_df.to_sql("stops", engine, if_exists="replace", index=False, dtype=column_types)
-engine.dispose()
+    write_sql_to_df(db_name, table_name, column_types)
 
-# Delete the stops.txt and GTFS.zip file
-files_to_delete = []
-files_to_delete.append(zip_path)
-files_to_delete.append(file_path)
+    # Delete the stops.txt and GTFS.zip file
+    files_to_delete.append(zip_path)
+    files_to_delete.append(file_path)
 
-for file in files_to_delete:
-    if os.path.exists(file):
-        os.remove(file)
+    delete_files(files_to_delete)
